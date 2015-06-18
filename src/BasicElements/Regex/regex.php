5dcb1372fe154276d7e23f3a653794abcfa5cde5
Isty001 <isty001@gmail.com>
2015-06-18 16:07:26 +0200

    PCRE

--- src/BasicElements/Regex/regex.php
+++ src/BasicElements/Regex/regex.php
@@ -10,4 +10,17 @@ print_r($matches);
 $string2 = '4F9nD10L';
 echo $string2 . PHP_EOL;
 
-echo preg_replace('[9nd]i', '888', $string2);
+echo preg_replace('[9nd]i', '888', $string2) .PHP_EOL;
+
+//
+if(preg_match("/\bw[eai]b\b/i", "PHP is a web scripting language")){
+    echo 'match' .PHP_EOL;
+} else {
+    echo 'No match'. PHP_EOL;
+}
+
+//
+preg_match_all("|<[^>]+>(.*)</[^>]+>|U", "<b>example: </b><div align=\"left\">this is a test</div>",
+    $out, PREG_SET_ORDER);
+
+print_r($out);
